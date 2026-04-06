3711424dab6bac3e0f5835f4912a3836a9ab5051
Satria Maulana Ridwan <izarubot@gmail.com>
2026-04-06 10:18:44 +0700

    Changes: Refining console command

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,17 @@ use App\Console\Commands\CheckExpiredBatches;
 use App\Console\Commands\SendDailySalesReport;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(CheckExpiredBatches::class)->dailyAt('07:00');
-Schedule::command(SendDailySalesReport::class)->dailyAt('21:00');
-Schedule::command('sanctum:prune-expired --hours=0')->daily();
+Schedule::command(CheckExpiredBatches::class)
+    ->dailyAt('07:00')
+    ->withoutOverlapping(1440)
+    ->onOneServer();
+
+Schedule::command(SendDailySalesReport::class)
+    ->dailyAt('21:00')
+    ->withoutOverlapping(1440)
+    ->onOneServer();
+
+Schedule::command('sanctum:prune-expired --hours=0')
+    ->dailyAt('00:00')
+    ->withoutOverlapping(60)
+    ->onOneServer();
